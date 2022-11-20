cd36dbf7825f3db51d1ae825b9afcc33248e53c9
Ercan ALTUN <salsel0147@gmail.com>
2022-11-20 18:04:52 +0300

    10.11.2022

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features",
                  glue = "stepDefinitions",
-                 tags = "@medunna",
+                 tags = "@testcase1",
                  dryRun = false)
 
 
---
+++ src/test/java/stepDefinitions/MyStepdefsTC1.java
@@ -0,0 +1,69 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.WebElement;
+import pages.ErcanPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+public class MyStepdefsTC1 {
+    ErcanPage ercanPage = new ErcanPage();
+
+    @Given("Kullanici url adresine gider")
+    public void kullaniciUrlAdresineGider() {
+        Driver.getDriver().get(ConfigReader.getProperty("medunna"));
+    }
+
+    @Then("Kullanici signin butonuna tiklar")
+    public void kullaniciSigninButonunaTiklar() {
+        ercanPage.enbas.click();
+        ercanPage.signIn.click();
+    }
+
+    @And("Kullanici gecerli bir username ve password girer")
+    public void kullaniciGecerliBirUsernameVePasswordGirer() {
+        ercanPage.username.sendKeys(ConfigReader.getProperty("doctor"));
+        ercanPage.password.sendKeys(ConfigReader.getProperty("drPassword"));
+        ercanPage.signInButton.click();
+    }
+
+    @And("Kullanici ana sayfada My Page sekmesine tiklar")
+    public void kullaniciAnaSayfadaMyPageSekmesineTiklar() {
+        ercanPage.myPages.click();
+    }
+
+    @And("My page sekmesinden My Appointments secenegini tiklar")
+    public void myPageSekmesindenMyAppointmentsSeceneginiTiklar() {
+        ercanPage.myAppointments.click();
+    }
+
+    @And("Appointments sayfasında  randevu listesini ve zaman dilimlerini   bilgilerini gorur")
+    public void appointmentsSayfasindaRandevuListesiniVeZamanDilimleriniBilgileriniGorur() {
+        List<WebElement> ravdevuListesi = ercanPage.randevuList;
+        assertTrue(!ravdevuListesi.isEmpty());
+        assertTrue(ercanPage.fromDate.isDisplayed());
+        assertTrue(ercanPage.toDate.isDisplayed());
+        ReusableMethods.waitFor(2);
+    }
+
+    @And("Sayfayi kapatir")
+    public void sayfayiKapatir() {
+        Driver.closeDriver();
+    }
+
+    //Test Case-2
+    @And("Kullanici  patient id, start date, end date, status bilgilerini gorur")
+    public void kullaniciPatientIdStartDateEndDateStatusBilgileriniGorur() {
+        assertTrue(ercanPage.patientId.isDisplayed()); //300975
+        assertTrue(ercanPage.startDate.isDisplayed()); //25/11/22 03:00
+        assertTrue(ercanPage.endDate.isDisplayed());   //25/11/22 04:00
+        assertTrue(ercanPage.status.isDisplayed());    //UNAPPROVED
+    }
+}
